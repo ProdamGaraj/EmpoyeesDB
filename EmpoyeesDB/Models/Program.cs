d0b29fd5103f6b59ed41699c324045bf1809318c
ProdamGaraj <prodgaraj@yandex.ru>
2020-12-06 23:18:18 +0300

    2task done

--- EmpoyeesDB/Models/Program.cs
+++ EmpoyeesDB/Models/Program.cs
@@ -9,7 +9,7 @@ namespace EmpoyeesDB
         static private EmployeesContext _context = new EmployeesContext();
         static void Main(string[] args)
         {
-            Console.WriteLine(GetHighSalaryWorkers());
+            Console.WriteLine(MoveWorkers());
         }
         static string GetEmployeesInformation()
         {
@@ -44,27 +44,38 @@ namespace EmpoyeesDB
                     e.LastName,
                     e.MiddleName,
                     e.JobTitle,
-                    e.DepartmentId,
-                    e.ManagerId,
                     e.HireDate,
-                    e.AddressId
+                    e.Salary
                 }).ToList();
 
             var sb = new StringBuilder();
 
             foreach (var e in employees)
             {
-                sb.AppendLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.DepartmentId} {e.ManagerId} {e.HireDate} {e.AddressId}");
+                sb.AppendLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.HireDate} {e.Salary}");
             }
             return sb.ToString().TrimEnd();
         }
 
         static string MoveWorkers()
         {
-            
-
-
             var sb = new StringBuilder();
+            _context.Towns.Add(new Towns() { Name = "Peteria" });
+            _context.SaveChanges();
+
+            var Browns = _context.Employees.Where(e => e.LastName.Equals("Brown")).Select(e=>e).ToList();
+            _context.Addresses.Add(new Addresses()
+            {
+                AddressText = "There is nothing to read.",
+                TownId = _context.Towns.AsEnumerable().LastOrDefault().TownId
+            });
+            _context.SaveChanges();
+            foreach (var e in Browns)
+            {
+                e.AddressId = _context.Addresses.AsEnumerable().LastOrDefault().AddressId;
+                _context.SaveChanges();
+                sb.AppendLine($"{e.FirstName} {e.LastName} {e.JobTitle} {e.HireDate} {e.Salary} {e.AddressId}");
+            }
 
             return sb.ToString().TrimEnd();
         }
