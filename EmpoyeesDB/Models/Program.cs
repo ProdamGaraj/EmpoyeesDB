e54f741c9822c822ab2b899e2099536b5a216c92
ProdamGaraj <prodgaraj@yandex.ru>
2020-12-06 18:20:25 +0300

    1st task in 3rd practise work added.

--- EmpoyeesDB/Models/Program.cs
+++ EmpoyeesDB/Models/Program.cs
@@ -9,7 +9,33 @@ namespace EmpoyeesDB
         static private EmployeesContext _context = new EmployeesContext();
         static void Main(string[] args)
         {
-            Console.WriteLine(GetEmployeesInformation());
+            Console.WriteLine(GetHighSalaryWorkers());
+        }
+
+        static string GetHighSalaryWorkers()
+        {
+            var employees = _context.Employees.
+                OrderBy(e => e.Salary).
+                Where(e => e.Salary >= 48000).
+                Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.MiddleName,
+                    e.JobTitle,
+                    e.DepartmentId,
+                    e.ManagerId,
+                    e.HireDate,
+                    e.AddressId
+                }).ToList();
+
+            var sb = new StringBuilder();
+
+            foreach (var e in employees)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.DepartmentId} {e.ManagerId} {e.HireDate} {e.AddressId}");
+            }
+            return sb.ToString().TrimEnd();
         }
 
         static string GetEmployeesInformation()
@@ -33,6 +59,5 @@ namespace EmpoyeesDB
 
             return sb.ToString().TrimEnd();
         }
-
     }
 }
