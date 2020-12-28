d1a5eea8e157cb78b9a1c93a50041b3a50dd148d
prodgaraj@yandex.ru <prodgaraj@yandex.ru>
2020-12-28 22:25:07 +0300

    LINQ SQL TYPE

--- EmpoyeesDB/Models/EmployeesContext.cs
+++ EmpoyeesDB/Models/EmployeesContext.cs
@@ -27,7 +27,7 @@ namespace EmpoyeesDB
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=PRODAMGARAJ;Database=Employees;Integrated Security=True;");
+                optionsBuilder.UseSqlServer(@"Server=DESKTOP-IT4ICGD\PRODAMGARAJ;Database=Employees;Integrated Security=True;");
             }
         }
 
--- EmpoyeesDB/Models/Program.cs
+++ EmpoyeesDB/Models/Program.cs
@@ -8,23 +8,21 @@ namespace EmpoyeesDB
 {
     class Program
     {
-        static private EmployeesContext _context = new EmployeesContext();
+        static public EmployeesContext _context { get; } = new EmployeesContext();
         static void Main(string[] args)
         {
             Console.WriteLine();//Type any of task names right here.
         }
         static string GetEmployeesInformation()
         {
-            var employees = _context.Employees
-                .OrderBy(e => e.EmployeeId)
-                .Select(e => new
-                {
-                    e.FirstName,
-                    e.LastName,
-                    e.MiddleName,
-                    e.JobTitle
-                })
-                .ToList();
+
+
+            var employees = (
+                from e in _context.Employees
+                orderby e.LastName
+                where e.Salary > 48000
+                select e
+                ).ToList();
             var sb = new StringBuilder();
 
             foreach (var e in employees)
@@ -37,18 +35,12 @@ namespace EmpoyeesDB
 
         static string GetHighSalaryWorkers()
         {
-            var employees = _context.Employees.
-                OrderBy(e => e.LastName).
-                Where(e => e.Salary >= 48000).
-                Select(e => new
-                {
-                    e.FirstName,
-                    e.LastName,
-                    e.MiddleName,
-                    e.JobTitle,
-                    e.HireDate,
-                    e.Salary
-                }).ToList();
+            var employees = (
+                from e in _context.Employees
+                orderby e.LastName
+                where e.Salary > 48000
+                select e
+            ).ToList();
 
             var sb = new StringBuilder();
 
@@ -65,13 +57,19 @@ namespace EmpoyeesDB
             _context.Towns.Add(new Towns() { Name = "Peteria" });
             _context.SaveChanges();
 
-            var Browns = _context.Employees.Where(e => e.LastName.Equals("Brown")).Select(e => e).ToList();
+            var Browns = (
+                from employee in _context.Employees
+                where employee.LastName == "Brown"
+                select employee
+            ).ToList();
+
             _context.Addresses.Add(new Addresses()
             {
                 AddressText = "There is nothing to read.",
                 TownId = _context.Towns.AsEnumerable().LastOrDefault().TownId
             });
             _context.SaveChanges();
+
             foreach (var e in Browns)
             {
                 e.AddressId = _context.Addresses.AsEnumerable().LastOrDefault().AddressId;
@@ -85,40 +83,34 @@ namespace EmpoyeesDB
         static string ProjectAudit()
         {
             var result =
-                _context.EmployeesProjects.Join(_context.Employees,
-                (ep => ep.EmployeeId),
-                (e => e.EmployeeId),
-                (ep, e) => new
-                {
-                    FirstName = e.FirstName,
-                    LastName = e.LastName,
-                    Manager = e.Manager,
-                    ProjectName = ep.Project.Name,
-                    StartDate = ep.Project.StartDate,
-                    EndDate = ep.Project.EndDate
-                }).
-                Where(e => e.StartDate.Year >= 2002 && e.StartDate.Year <= 2005).
-                ToList();
+                from Employees in _context.Employees
+                join EmployeesProjects in _context.EmployeesProjects on Employees.EmployeeId equals EmployeesProjects.EmployeeId
+                join Projects in _context.Projects on EmployeesProjects.ProjectId equals Projects.ProjectId
+                where EmployeesProjects.Project.StartDate.Year >= 2002 && EmployeesProjects.Project.StartDate.Year <= 2005
+                select new { Employees, Projects };
+
+
             string NullEndDate = "НЕ ЗАВЕРШЕН\n";
             var sb = new StringBuilder();
             int i = 0;
             int j = 0;
             foreach (var r in result)
             {
-                if (i == 0 || (r.FirstName != result[i - 1].FirstName && r.LastName != result[i - 1].LastName && j > 0 && j < 5))
+                if (i == 0 || (r.Employees.FirstName != result.AsEnumerable().ElementAt(i - 1).Employees.FirstName && r.Employees.LastName != result.AsEnumerable().ElementAt(i - 1).Employees.LastName && j > 0 && j < 5))
                 {
-                    if (r.EndDate == null)
+                    if (r.Projects.EndDate != null)
                     {
-                        sb.Append($"{r.FirstName} {r.LastName} {r.Manager.FirstName} {r.Manager.LastName} {r.ProjectName} {r.StartDate} {NullEndDate}");
+                        sb.Append($"{r.Employees.FirstName} {r.Employees.LastName} {r?.Employees.Manager.FirstName} {r?.Employees.Manager.LastName} {r.Projects.Name} {r.Projects.StartDate} {NullEndDate}");
                     }
                     else
                     {
-                        sb.Append($"{r.FirstName} {r.LastName} {r.Manager.FirstName} {r.Manager.LastName} {r.ProjectName} {r.StartDate} {r.EndDate}" + "\n");
+                        sb.Append($"{r.Employees.FirstName} {r.Employees.LastName} {r?.Employees.Manager.FirstName} {r?.Employees.Manager.LastName} {r.Projects.Name} {r.Projects.StartDate} {r.Projects.EndDate}" + "\n");
                     }
                     j++;
                 }
                 i++;
             }
+
             return sb.ToString().TrimEnd();
         }
 
@@ -126,24 +118,17 @@ namespace EmpoyeesDB
         {
             int id = Convert.ToInt32(Console.ReadLine());
             var result =
-                _context.EmployeesProjects.Join(_context.Employees,
-                (ep => ep.EmployeeId),
-                (e => e.EmployeeId),
-                (ep, e) => new
-                {
-                    EmployeeId = e.EmployeeId,
-                    FirstName = e.FirstName,
-                    LastName = e.LastName,
-                    MiddleName = e.MiddleName,
-                    ProjectName = ep.Project.Name
-                }).
-                Where(e => e.EmployeeId == id).
-                ToList();
+                from Employees in _context.Employees
+                join EmployeesProjects in _context.EmployeesProjects on Employees.EmployeeId equals EmployeesProjects.EmployeeId
+                join Projects in _context.Projects on EmployeesProjects.ProjectId equals Projects.ProjectId
+                where (Employees.EmployeeId == id)
+                select new { Employees, Projects };
+
             var sb = new StringBuilder();
-            sb.Append($"{result[0].FirstName} {result[0].LastName} {result[0].MiddleName} " + " \n");
+            sb.Append($"{result.AsEnumerable().ElementAt(0).Employees.FirstName} {result.AsEnumerable().ElementAt(0).Employees.LastName} {result.AsEnumerable().ElementAt(0).Employees.MiddleName} " + " \n");
             foreach (var r in result)
             {
-                sb.Append($"{r.ProjectName}" + "\n");
+                sb.Append($"{r.Projects.Name}" + "\n");
             }
             return sb.ToString().TrimEnd();
         }
@@ -151,13 +136,15 @@ namespace EmpoyeesDB
         static string LitleDepartaments()
         {
             var result =
-                _context.Departments.Where(d => d.Employees.Count < 5).ToList();
+                from Departments in _context.Departments
+                where Departments.Employees.Count < 5
+                select new { Departments };
 
             var sb = new StringBuilder();
 
             foreach (var r in result)
             {
-                sb.Append($"{r.Name}" + "\n");
+                sb.Append($"{r.Departments.Name}" + "\n");
             }
 
             return sb.ToString().TrimEnd();
@@ -171,36 +158,40 @@ namespace EmpoyeesDB
             Console.WriteLine("Vvedite % povisheniya zarplat?");
             int IncreasePercent = Convert.ToInt32(Console.ReadLine());
             var result =
-                _context.Employees.Where(e => e.DepartmentId == DepartamentId).Select(e => e);
+                from Employees in _context.Employees
+                where Employees.DepartmentId == DepartamentId
+                select new { Employees };
 
             var sb = new StringBuilder();
 
             foreach (var r in result)
             {
-                sb.Append($"{r.FirstName} {r.LastName} {r.Salary} " + "\n");
-                r.Salary *= (decimal)((IncreasePercent + 100) / 100f);
+                sb.Append($"{r.Employees.FirstName} {r.Employees.LastName} {r.Employees.Salary} " + "\n");
+                r.Employees.Salary *= (decimal)((IncreasePercent + 100) / 100f);
             }
             sb.Append("New salaries \n");
             _context.SaveChanges();
             foreach (var r in result)
             {
-                sb.Append($"{r.FirstName} {r.LastName} {r.Salary} " + "\n");
+                sb.Append($"{r.Employees.FirstName} {r.Employees.LastName} {r.Employees.Salary} " + "\n");
             }
             return sb.ToString().TrimEnd();
         }
 
         static string PanicMethod()
-            {
+        {
             string DepartmentName = Console.ReadLine();
             var department = _context.Departments.First(d => d.Name == DepartmentName);
-            Employees TempEmpl = new Employees { 
+            Employees TempEmpl = new Employees
+            {
                 FirstName = "Temp",
                 LastName = "Temp",
                 JobTitle = "Temp",
                 DepartmentId = 2,
                 HireDate = new DateTime(2008, 5, 1),
-                Salary=0 };
-            Departments TempDep = new Departments { Name = "Temp", Manager=TempEmpl};
+                Salary = 0
+            };
+            Departments TempDep = new Departments { Name = "Temp", Manager = TempEmpl };
             _context.Departments.Add(TempDep);
             _context.SaveChanges();
             TempEmpl.Department = TempDep;
@@ -210,25 +201,37 @@ namespace EmpoyeesDB
                 Console.WriteLine("It`s nothing to search in here.");
                 return null;
             }
-            var employees = _context.Employees.Where(e => e.Department.Name == DepartmentName).ToList();
+            var employees =
+                from Employees in _context.Employees
+                where Employees.Department.Name == DepartmentName
+                select new { Employees };
             foreach (var e in employees)
             {
-                e.Department=_context.Departments.First(d=>d==TempDep);
+                e.Employees.Department = _context.Departments.First(d => d == TempDep);
             }
             department.Manager = null;
             _context.SaveChanges();
             _context.Departments.Remove(department);
             _context.SaveChanges();
             return "Keep panic away it`s gone.";
-            }
+        }
         static string Town404()
         {
             string TownName = Console.ReadLine();
 
-            var TownToDelete = _context.Towns.First(d => d.Name == TownName);
+            var TownToDelete =
+                from Towns in _context.Towns
+                where Towns.Name == TownName
+                select Towns;
             Addresses Adr = new Addresses { AddressText = "Temp" };
-            var AddressesToDelete = _context.Addresses.Where(a=>a.Town==TownToDelete).ToList();
-            var Employees = _context.Employees.Where(e => e.Address.Town==TownToDelete).ToList();
+            var AddressesToDelete =
+                from Adresses in _context.Addresses
+                where Adresses.Town == TownToDelete
+                select Adresses;
+            var Employees = 
+                from e in _context.Employees
+                where e.Address.Town == TownToDelete
+                select  e;
             foreach (var e in Employees)
             {
                 e.Address = Adr;
@@ -239,10 +242,12 @@ namespace EmpoyeesDB
                 _context.Addresses.Remove(a);
             }
             _context.SaveChanges();
-            _context.Towns.Remove(TownToDelete);
+            foreach (var t in TownToDelete)
+            {
+                _context.Towns.Remove(t);
+            }
             _context.SaveChanges();
-            return TownName+" deleted";
+            return TownName + " deleted";
         }
-
     }
 }
